a9df38945cdda51a0fc951dbdec090612136de77
alejandro-du <alejandro.d.a@gmail.com>
2016-11-03 23:57:06 +0200

    Added Companies CRUD

--- admin-application/src/main/java/com/example/admin/CompanyService.java
+++ admin-application/src/main/java/com/example/admin/CompanyService.java
@@ -2,12 +2,25 @@ package com.example.admin;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.hateoas.Resources;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient("biz-application")
+@RequestMapping("/companies")
 public interface CompanyService {
 
-    @RequestMapping("/companies")
+    @RequestMapping
     Resources<Company> list();
+
+    @RequestMapping(method = RequestMethod.POST)
+    void add(@RequestBody Company company);
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    void update(@PathVariable("id") Long id, @RequestBody Company company);
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    void delete(@PathVariable("id") Long id);
 
 }
--- admin-application/src/main/java/com/example/admin/VaadinUI.java
+++ admin-application/src/main/java/com/example/admin/VaadinUI.java
@@ -1,23 +1,21 @@
 package com.example.admin;
 
 import com.vaadin.annotations.Theme;
-import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.Grid;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.themes.ValoTheme;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.vaadin.crudui.impl.crud.GridBasedCrudComponent;
 
 import java.util.Collection;
 
 @SpringUI
 @Theme(ValoTheme.THEME_NAME)
-public class VaadinUI extends UI {
+public class VaadinUI extends UI implements GridBasedCrudComponent.GridCrudListener<Company> {
 
     private final CompanyService companyService;
-
-    private Grid grid = new Grid();
 
     @Autowired
     public VaadinUI(CompanyService companyService) {
@@ -26,10 +24,36 @@ public class VaadinUI extends UI {
 
     @Override
     protected void init(VaadinRequest request) {
-        Collection<Company> companies = companyService.list().getContent();
-        grid.setContainerDataSource(new BeanItemContainer<>(Company.class, companies));
-        grid.setColumns("name");
-        setContent(grid);
+        GridBasedCrudComponent companiesCrud = new GridBasedCrudComponent<>(Company.class, this);
+        companiesCrud.showAllOptions();
+        companiesCrud.setNewFormVisiblePropertyIds("name");
+        companiesCrud.setEditFormDisabledPropertyIds("id");
+
+        HorizontalLayout mainLayout = new HorizontalLayout(companiesCrud);
+        mainLayout.setSizeFull();
+        mainLayout.setMargin(true);
+        mainLayout.setSpacing(true);
+        setContent(mainLayout);
+    }
+
+    @Override
+    public Collection<Company> refreshTable() {
+        return companyService.list().getContent();
+    }
+
+    @Override
+    public void add(Company company) {
+        companyService.add(company);
+    }
+
+    @Override
+    public void update(Company company) {
+        companyService.update(company.getId(), company);
+    }
+
+    @Override
+    public void delete(Company company) {
+        companyService.delete(company.getId());
     }
 
 }
