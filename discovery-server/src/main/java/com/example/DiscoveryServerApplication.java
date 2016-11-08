b30c939c3a63b92a3c04b344d01fcde9f019dc66
alejandro-du <alejandro.d.a@gmail.com>
2016-11-08 14:25:22 +0200

    Renamed eureka-server to discovery-server

--- discovery-server/src/main/java/com/example/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/example/DiscoveryServerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication {
+public class DiscoveryServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(DiscoveryServerApplication.class, args);
     }
 }
