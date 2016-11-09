0458efcff0208365a9bd54ecff1f05dde73bb0cc
alejandro-du <alejandro.d.a@gmail.com>
2016-11-09 02:07:43 +0200

    Removed lombok

--- admin-application/src/main/java/com/example/admin/Company.java
+++ admin-application/src/main/java/com/example/admin/Company.java
@@ -1,12 +1,25 @@
 package com.example.admin;
 
-import lombok.Data;
-
-@Data
 public class Company {
 
     private Long id;
 
     private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
