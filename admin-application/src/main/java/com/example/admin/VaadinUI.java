9bbae0cd2acd83d1430634a68b34c220a1fd61a7
alejandro-du <alejandro.d.a@gmail.com>
2016-11-14 21:54:48 +0200

    Updated Crud UI add-on version

--- admin-application/src/main/java/com/example/admin/VaadinUI.java
+++ admin-application/src/main/java/com/example/admin/VaadinUI.java
@@ -7,7 +7,7 @@ import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.themes.ValoTheme;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.vaadin.crudui.impl.crud.GridBasedCrudComponent;
+import org.vaadin.crudui.crud.impl.GridBasedCrudComponent;
 
 @SpringUI
 @Theme(ValoTheme.THEME_NAME)
@@ -23,12 +23,13 @@ public class VaadinUI extends UI {
     @Override
     protected void init(VaadinRequest request) {
         GridBasedCrudComponent<Company> crud = new GridBasedCrudComponent<>(Company.class);
-        crud.setVisiblePropertyIds("name", "phone", "email", "address");
+        crud.getCrudFormFactory().setVisiblePropertyIds("name", "phone", "email", "address");
         crud.setOperations(
+                () -> companyService.findAll().getContent(),
                 company -> companyService.add(company),
                 company -> companyService.update(company.getId(), company),
-                company -> companyService.delete(company.getId()),
-                () -> companyService.findAll().getContent());
+                company -> companyService.delete(company.getId())
+        );
 
         HorizontalLayout mainLayout = new HorizontalLayout(crud);
         mainLayout.setSizeFull();
