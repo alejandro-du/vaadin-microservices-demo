0bd3527037b4907b8e5234ab8f3cd9425e26a61e
alejandro-du <alejandro.d.a@gmail.com>
2016-11-17 01:04:23 +0200

    Added sticky session rule

--- proxy-server/src/main/java/com/example/ProxyServerApplication.java
+++ proxy-server/src/main/java/com/example/ProxyServerApplication.java
@@ -1,29 +1,17 @@
 package com.example;
 
-import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.RoundRobinRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
-@RibbonClient(name = "proxy-server-ribbon-client", configuration = ProxyServerApplication.RibbonConfig.class)
 public class ProxyServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProxyServerApplication.class, args);
-    }
-
-    static class RibbonConfig {
-        @Bean
-        public IRule defaultRule() {
-            return new RoundRobinRule();
-        }
     }
 
 }
---
+++ proxy-server/src/main/java/com/example/StickySessionRule.java
@@ -0,0 +1,57 @@
+package com.example;
+
+import com.netflix.loadbalancer.ClientConfigEnabledRoundRobinRule;
+import com.netflix.loadbalancer.Server;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.Cookie;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Alejandro Duarte.
+ */
+@Component
+public class StickySessionRule extends ClientConfigEnabledRoundRobinRule {
+
+    public static final String COOKIE_NAME = StickySessionRule.class.getSimpleName();
+
+    @Override
+    public Server choose(Object key) {
+        Optional<Cookie> cookie = getCookie();
+
+        if (cookie.isPresent()) {
+            Cookie hash = cookie.get();
+            List<Server> servers = getLoadBalancer().getReachableServers();
+            Optional<Server> serverFound = servers.stream().filter(s -> hash.getValue().equals("" + s.hashCode())).findFirst();
+
+            if (serverFound.isPresent()) {
+                return serverFound.get();
+            }
+        }
+
+        return addServer(key);
+    }
+
+    private Optional<Cookie> getCookie() {
+        Cookie[] cookies = RequestContext.getCurrentContext().getRequest().getCookies();
+        if (cookies != null) {
+            return Arrays.stream(cookies)
+                    .filter(c -> c.getName().equals(COOKIE_NAME))
+                    .findFirst();
+        }
+
+        return Optional.empty();
+    }
+
+    private Server addServer(Object key) {
+        Server server = super.choose(key);
+        Cookie newCookie = new Cookie(COOKIE_NAME, "" + server.hashCode());
+        newCookie.setPath("/");
+        RequestContext.getCurrentContext().getResponse().addCookie(newCookie);
+        return server;
+    }
+
+}
