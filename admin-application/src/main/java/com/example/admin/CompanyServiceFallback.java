cb4f52cf0c61dfb6929c1dd4b863905e3703d176
Alejandro Duarte <alejandro@vaadin.com>
2017-11-23 01:12:27 +0200

    Configuration tuning

--- admin-application/src/main/java/com/example/admin/CompanyServiceFallback.java
+++ admin-application/src/main/java/com/example/admin/CompanyServiceFallback.java
@@ -14,7 +14,7 @@ public class CompanyServiceFallback implements CompanyService {
 
     @Override
     public Resources<Company> findAll() {
-        Notification.show("Cannot connect to biz-application. Please try again later.",
+        Notification.show("Operation not available. Please try again later.",
                 Notification.Type.ERROR_MESSAGE);
         return new Resources<>(Collections.emptyList());
     }
